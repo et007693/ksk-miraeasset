c29cba78cba5cc98cdcc43fc112266774eb705d5
Song <84127378+et007693@users.noreply.github.com>
2023-09-06 20:35:35 +0900

    Add files via upload

---
+++ 송창용/수익률_반영/main.py
@@ -0,0 +1,144 @@
+import os
+import time
+import torch
+import argparse
+import pandas as pd
+
+from model import SASRec
+from utils import *
+
+def str2bool(s):
+    if s not in {'false', 'true'}:
+        raise ValueError('Not a valid boolean string')
+    return s == 'true'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', required=True)
+parser.add_argument('--train_dir', required=True)
+parser.add_argument('--batch_size', default=256, type=int)
+parser.add_argument('--lr', default=0.001, type=float)
+parser.add_argument('--maxlen', default=12, type=int)
+parser.add_argument('--hidden_units', default=50, type=int)
+parser.add_argument('--num_blocks', default=2, type=int)
+parser.add_argument('--num_epochs', default=15, type=int)
+parser.add_argument('--num_heads', default=1, type=int)
+parser.add_argument('--dropout_rate', default=0.5, type=float)
+parser.add_argument('--l2_emb', default=0.0, type=float)
+parser.add_argument('--device', default='cuda', type=str)
+parser.add_argument('--inference_only', default=False, type=str2bool)
+parser.add_argument('--state_dict_path', default=None, type=str)
+
+args = parser.parse_args()
+if not os.path.isdir(args.dataset + '_' + args.train_dir):
+    os.makedirs(args.dataset + '_' + args.train_dir)
+with open(os.path.join(args.dataset + '_' + args.train_dir, 'args.txt'), 'w') as f:
+    f.write('\n'.join([str(k) + ',' + str(v) for k, v in sorted(vars(args).items(), key=lambda x: x[0])]))
+f.close()
+
+if __name__ == '__main__':
+    dataset = data_partition(args.dataset)
+
+    seed = 32
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if args.device == 'cuda':
+        torch.cuda.manual_seed_all(seed)
+
+    # user_train - 구매목록
+    # user_train_side - 구매목록의 보조정보
+    # item_side - item, 보조정보 쌍
+    [user_train, user_valid, user_test, usernum, itemnum,
+    user_train_side, user_valid_side, user_test_side, item_side,
+    umap, smap] = dataset
+
+    num_batch = len(user_train) // args.batch_size 
+    cc = 0.0
+    for u in user_train:
+        cc += len(user_train[u])
+    print('average sequence length: %.2f' % (cc / len(user_train)))
+    
+    f = open(os.path.join(args.dataset + '_' + args.train_dir, 'log.txt'), 'w')
+    
+    sampler = WarpSampler(user_train, user_train_side, usernum, itemnum, item_side, batch_size=args.batch_size, maxlen=args.maxlen, n_workers=3)
+    
+    model = SASRec(usernum, itemnum, item_side, args).to(args.device) 
+    for name, param in model.named_parameters():
+        try:
+            torch.nn.init.xavier_normal_(param.data)
+        except:
+            pass 
+
+    model.train() 
+    
+    epoch_start_idx = 1
+    if args.state_dict_path is not None:
+        try:
+            model.load_state_dict(torch.load(args.state_dict_path, map_location=torch.device(args.device)))
+            tail = args.state_dict_path[args.state_dict_path.find('epoch=') + 6:]
+            epoch_start_idx = int(tail[:tail.find('.')]) + 1
+        except: 
+            print('failed loading state_dicts, pls check file path: ', end="")
+            print(args.state_dict_path)
+            print('pdb enabled for your quick check, pls type exit() if you do not need it')
+            import pdb; pdb.set_trace()   
+    
+    if args.inference_only:
+        model.eval()
+        t_test = evaluate(model, dataset, args)
+        print('test (NDCG@10: %.4f, HR@10: %.4f)' % (t_test[0], t_test[1]))
+
+    bce_criterion = torch.nn.BCEWithLogitsLoss() 
+    adam_optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, betas=(0.9, 0.98))
+    
+    T = 0.0
+    t0 = time.time()
+    result= pd.DataFrame(columns=['NDCG_val', 'HR_val','NDCG_test', 'HR_test'])
+
+    for epoch in range(epoch_start_idx, args.num_epochs + 1):
+        if args.inference_only: break 
+        for step in range(num_batch): 
+            u, seq, pos, neg, seq_side, pos_side, neg_side = sampler.next_batch() 
+            u, seq, pos, neg, seq_side, pos_side, neg_side = (
+            np.array(u), np.array(seq), np.array(pos), np.array(neg), 
+            np.array(seq_side), np.array(pos_side), np.array(neg_side))
+            pos_logits, neg_logits = model(u, seq, pos, neg, seq_side)
+            pos_labels, neg_labels = torch.ones(pos_logits.shape, device=args.device), torch.zeros(neg_logits.shape, device=args.device)
+            adam_optimizer.zero_grad()
+            indices = np.where(pos != 0)
+            loss = bce_criterion(pos_logits[indices], pos_labels[indices])
+            loss += bce_criterion(neg_logits[indices], neg_labels[indices])
+            for param in model.item_emb.parameters(): loss += args.l2_emb * torch.norm(param)
+            loss.backward()
+            adam_optimizer.step()
+            print("loss in epoch {} iteration {}: {}".format(epoch, step, loss.item())) 
+    
+        if epoch % 5 == 0:
+            model.eval()
+            t1 = time.time() - t0
+            T += t1
+            print('Evaluating', end='')
+            t_test = evaluate(model, dataset, args)
+            t_valid = evaluate_valid(model, dataset, args)
+            print('epoch:%d, time: %f(s), valid (NDCG@10: %.4f, HR@10: %.4f), test (NDCG@10: %.4f, HR@10: %.4f)'
+                    % (epoch, T, t_valid[0], t_valid[1], t_test[0], t_test[1]))
+            data_to_insert = {'NDCG_val':t_valid[0], 'HR_val':t_valid[1],'NDCG_test':t_test[0], 'HR_test':t_test[1]}
+            result = result.append(data_to_insert, ignore_index=True)
+            f.write(str(t_valid) + ' ' + str(t_test) + '\n')
+            f.flush()
+            t0 = time.time()
+            model.train()
+    
+        if epoch == args.num_epochs:
+            folder = args.dataset + '_' + args.train_dir
+            fname = 'SASRec.epoch={}.lr={}.layer={}.head={}.hidden={}.maxlen={}.pth'
+            fname = fname.format(args.num_epochs, args.lr, args.num_blocks, args.num_heads, args.hidden_units, args.maxlen)
+            torch.save(model.state_dict(), os.path.join(folder, fname))
+
+    
+    recommend_data = recommend_items(model, dataset, args)
+
+    f.close()
+    sampler.close()
+    recommend_data.to_csv('./%s.csv'% args.dataset, index=False)
+    result.to_csv('./result/%s.csv'% args.dataset, index=False)
+    print("Done")
